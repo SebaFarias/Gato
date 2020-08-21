5816737b0f8f35cda66a6b1a39fdbfc5b3b061e0
S F <sebastian.fariasb@usach.cl>
2020-08-20 20:48:23 -0400

    MiniMax Algorithm implemented on bot's  plays

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-const board = document.querySelector('.board'),
-cells = board.querySelectorAll('.cell'),
-modeOptions = document.querySelectorAll('.option'),
-botMenu = document.querySelector('#choose-mark'),
-finalMessage = document.querySelector('.final-message'),
-winningCombinations = [
+const winningCombinations = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -12,28 +7,35 @@ winningCombinations = [
     [2,5,8],
     [0,4,8],
     [2,4,6]
-],
-gamemodes = {
+]
+const gamemodes = {
     local2P: 'local-2P',
     bot: 'local-bot'
 }
-let gameMode = gamemodes.local2P,
-option = 'local-2P'
-matchStarted = false,
-botMark = 'o'
+const board = document.querySelector('.board')
+const cells = board.querySelectorAll('.cell')
+const modeOptions = document.querySelectorAll('.option')
+const botMenu = document.querySelector('#choose-mark')
+const finalMessage = document.querySelector('.final-message')
+
+let gameMode = gamemodes.local2P
+let option = 'local-2P'
+let matchStarted = false
+let botMark = 'o'
 
 cells.forEach( cell => { cell.addEventListener('click' , (e) => {makeMove(e.target)})})
 modeOptions.forEach( option => {option.addEventListener('click', (e) => {changeGameMode(e)})})
 botMenu.addEventListener('click', (e) => {handleBotMenu(e)})
 finalMessage.addEventListener('click', (e) => {restart(e)})
 
+//*************************     Alteraciones al Tablero     *************************
 const makeMove = (cell) => {
     if(cell!== false && isFree(cell)){
         if(board.classList.contains('x')){
             cell.classList.add('x')
-        }else{
+        } else{
             cell.classList.add('o')
-        }
+        }              
         matchStarted = true
         switchTurns()
     }
@@ -42,81 +44,24 @@ const makeMove = (cell) => {
     else if(isFinnished(cells))showFinalMessage('Es un empate!')
     if(!isFinnished(cells))botMove()
 }
+const switchTurns = () => {
+    board.classList.toggle('x')
+    board.classList.toggle('o')
+}
+const cleanBoard = () => {
+    cells.forEach(cell=>{
+        cell.classList.remove('x')
+        cell.classList.remove('o')
+    })
+}
+const resetTurns = () => {
+    board.classList.remove('o')
+    board.classList.add('x')
+}
+//*************************      Consultas de estados       ************************* 
 const isBotsTurn = () => {
     if(gameMode === gamemodes.bot && board.classList.contains(botMark))return true
     return false
-}
-const botMove = () => {
-    if(isBotsTurn()){
-        let choosenCell,
-        minDepth =99 ,
-        bestScore = -2
-        cells.forEach( (cell , index) =>{
-            if(isFree(cell)){
-                let eval = easyBot(tryHere(cells,index,true),false,0)                
-                //console.log(`${eval[0]} > ${bestScore} : ${eval[0] > bestScore}`);
-                //console.log(`${eval[0]} === ${bestScore} && ${eval[1]} < ${minDepth} : ${eval[0] === bestScore && eval[1] < minDepth}`);
-                if(eval[0] > bestScore ){
-                    bestScore = eval[0]
-                    choosenCell = cell
-                }else{
-                    if(eval[0] === bestScore && eval[1] < minDepth){
-                        minDepth = eval[1]
-                        choosenCell = cell
-                    }    
-                }
-            }
-        })
-        makeMove(choosenCell)
-    }
-}
-const tryHere = ( _board , i , isBot) => {
-    let simpleBoard = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-    if(typeof(_board[0]) !== "string" ){
-        _board.forEach( (cell,index) => {
-            if(cell.classList.contains('x')) simpleBoard[index] = 'x'
-            if(cell.classList.contains('o')) simpleBoard[index] = 'o'
-            if(index === i) simpleBoard[index] = isBot? botMark : otherMark(botMark)
-        })
-    }else{
-        simpleBoard = _board
-        simpleBoard[i] = isBot? botMark : otherMark(botMark)
-    }
-    return simpleBoard
-}
-const otherMark = mark => {
-    return mark === 'x' ? 'o' : 'x' 
-}
-const easyBot = (_board,isMaximizer,depht) => {
-    let winner = checkWin(_board)
-    if(isFinnished(_board)){
-        if(winner === false)return [0,depht]
-        return winner === botMark? [1,depht] : [-1,depht]
-    }
-    if(isMaximizer){
-        let bestScore = -2
-        _board.forEach((cell,index) => {
-            if(cell === ' '){
-                let eval = easyBot(tryHere(_board,index,true),false,depht+1)
-                bestScore = Math.max(eval[0],bestScore)
-            }
-        })
-        return [bestScore,Math.max(depht,eval[1])]
-    }else{
-        let bestScore = 2
-        _board.forEach((cell,index) => {
-            if(cell === ' '){
-                let eval = easyBot(tryHere(_board,index,false),true,depht+1)
-                bestScore = Math.min(eval[0],bestScore)
-            }
-        })
-        return [bestScore,Math.max(depht,eval[1])]
-    }
-}
-const handleBotMenu = (event) => {
-    let clicked = event.target 
-    if(clicked.classList.contains('bot-start-btn'))newBotGame(clicked)
-    else if(!isFinnished(cells))botMenu.classList.remove('show')
 }
 const isFree = (cell) =>{
     if(typeof(cell) === 'string'){
@@ -126,25 +71,6 @@ const isFree = (cell) =>{
         if(cell.classList.contains('x') || cell.classList.contains('o')) return false
     }
     return true
-}
-const changeGameMode = (event) => {
-    const selectedOption = event.target.parentElement
-    if(selectedOption.classList.contains('local-2P')){
-        option = gamemodes.local2P
-        showFinalMessage('¿Comenzar nueva Partida?')
-    }
-    if(selectedOption.classList.contains('local-bot'))botMenu.classList.add('show');
-}
-const newBotGame = (playerMark) => {
-    botMark = playerMark.classList.contains('x')? 'o' : 'x'
-    matchStarted = true
-    gameMode = gamemodes.bot
-    option = gamemodes.bot
-    cleanBoard()
-    resetTurns()
-    botMove()
-    showSelectedMode('.local-bot')
-    botMenu.classList.remove('show')
 }
 const checkWin = (cellsToCheck) => {
     if(typeof(cellsToCheck[0]) === 'string'){
@@ -179,16 +105,10 @@ const isFinnished = (cellsToCheck) => {
     }
     return true
 }
-const switchTurns = () => {
-    board.classList.toggle('x')
-    board.classList.toggle('o')
+const otherMark = mark => {
+    return mark === 'x' ? 'o' : 'x' 
 }
-const won = (winner) => {
-    showFinalMessage(`${winner} ha ganado !`)
-}
-const draw = () => {
-    showFinalMessage('Es un empate!')
-}
+//*************************         Modos de Juego           *************************
 const restart = (e) => {
     if(e.target.classList.contains('restart-btn')){
         if(option === gamemodes.local2P){
@@ -206,23 +126,103 @@ const restart = (e) => {
     }
     if(!isFinnished(cells))finalMessage.classList.remove('show')
 }
+const newBotGame = (playerMark) => {
+    botMark = playerMark.classList.contains('x')? 'o' : 'x'
+    matchStarted = true
+    gameMode = gamemodes.bot
+    option = gamemodes.bot
+    cleanBoard()
+    resetTurns()
+    botMove()
+    showSelectedMode('.local-bot')
+    botMenu.classList.remove('show')
+}
+//*************************       Movimientos del Bot        *************************
+const botMove = () => {
+    if(isBotsTurn()){
+        let choosenCell,
+        bestScore = -2
+        cells.forEach( (cell , index) =>{
+            if(isFree(cell)){
+                let moveScore = minimax(tryHere(cells,index,true),false)                
+                if(moveScore > bestScore ){
+                    bestScore = moveScore
+                    choosenCell = cell
+                }
+            }
+        })
+        makeMove(choosenCell)
+    }
+}
+const minimax = (_board,isMaximizer) => {
+    let winner = checkWin(_board)
+    if(isFinnished(_board)){
+        if(winner === false){
+            return 0    
+        }
+        return winner === botMark? 1 : -1
+    }
+    if(isMaximizer){
+        let bestScore = -2
+        _board.forEach((cell,index) => {
+            if(cell === ' '){
+                let moveScore = minimax(tryHere(_board,index,true),false)                
+                bestScore = Math.max(moveScore,bestScore)
+            }
+        })
+        return bestScore
+    }else{
+        let bestScore = 2
+        _board.forEach((cell,index) => {
+            if(cell === ' '){
+                let moveScore = minimax(tryHere(_board,index,false),true)
+                bestScore = Math.min(moveScore,bestScore)
+            }
+        })
+        return bestScore
+    }
+}
+const tryHere = ( _board , i , isBot) => {
+    let simpleBoard = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    if(typeof(_board[0]) !== 'string' ){
+        _board.forEach( (cell,index) => {
+            if(cell.classList.contains('x')) simpleBoard[index] = 'x'
+            if(cell.classList.contains('o')) simpleBoard[index] = 'o'
+            if(index === i) simpleBoard[index] = isBot? botMark : otherMark(botMark)
+        })
+    }else{
+        simpleBoard = [..._board]
+        simpleBoard[i] = isBot? botMark : otherMark(botMark)
+    }
+    return simpleBoard
+}
+//*************************              Menus              *************************
+const changeGameMode = (event) => {
+    const selectedOption = event.target.parentElement
+    if(selectedOption.classList.contains('local-2P')){
+        option = gamemodes.local2P
+        showFinalMessage('¿Comenzar nueva Partida?')
+    }
+    if(selectedOption.classList.contains('local-bot'))botMenu.classList.add('show');
+}
 const showSelectedMode = (mode) => {
     modeOptions.forEach(option => {
         option.querySelector('.indicator').classList.remove('selected')
     })
     document.querySelector(mode).querySelector('.indicator').classList.add('selected')
 }
-const cleanBoard = () => {
-    cells.forEach(cell=>{
-        cell.classList.remove('x')
-        cell.classList.remove('o')
-    })
-}
-const resetTurns = () => {
-    board.classList.remove('o')
-    board.classList.add('x')
+const handleBotMenu = (event) => {
+    let clicked = event.target 
+    if(clicked.classList.contains('bot-start-btn'))newBotGame(clicked)
+    else if(!isFinnished(cells))botMenu.classList.remove('show')
 }
 const showFinalMessage = (mensaje) => {
     finalMessage.querySelector('h1').innerText = mensaje
     finalMessage.classList.add('show')
+}
+const won = (winner) => {
+    showFinalMessage(`${winner} ha ganado !`)
+}
+const draw = () => {
+    showFinalMessage('Es un empate!')
 }
