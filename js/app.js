80672b63c566fcb2cc1a70bde6b5f5dcaa317aa3
S F <sebastian.fariasb@usach.cl>
2020-10-16 19:11:00 -0300

    not closing menu on copy to clipboard

--- js/app.js
+++ js/app.js
@@ -34,6 +34,7 @@ const boardHandler = event =>{
 const menuHandler = event => {
     switch(event.target.classList[0]){
         case 'show': // click on menu's background
+            if(document.getElementById('copyBtn'))return
             if(!game.checkwin()){
                 menu.newMenu()
                 menu.setOption(game.getGameMode)
